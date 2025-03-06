d2dd8f2c747327f836b2505dfae6019db8ce7da3
Pacheko <patrickoluochex@gmail.com>
2025-03-06 15:13:39 +0300

    update mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 
 export default function Navbar() {
   return (
+
+<>
+     {/* mobile nav */}
+     <ul className='text-white lg:hidden h-full bg-red-500 w-3/4 text-5xl gap-2 flex flex-col fixed top-0'>
+
+     <li>Pricing</li>
+     <li>Sign Up</li>
+     <li>Login</li>
+   </ul>
     <div className='bg-[#1c1b18] flex px-4 pt-4 justify-between '>
 
-      {/* mobile nav */}
-      <ul className='text-white lg:hidden h-[100vw] bg-red-500 w-3/4 text-5xl gap-2 flex flex-col'>
-        
-        <li>Pricing</li>
-        <li>Sign Up</li>
-        <li>Login</li>
-      </ul>
+     
 
 
       <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
@@ -19,6 +22,7 @@ export default function Navbar() {
       <div className=' text-white top-0 py-2 text-2xl italic '>Keja<span className='text-[#F1F252]'>.</span></div>
 
     </div>
+    </>
 
   )
 }
