904df44dc22431f8d743cf1beed0b9dbf927c0cf
Pacheko <patrickoluochex@gmail.com>
2025-03-06 12:14:55 +0300

    align nav

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,8 @@ export default function Home() {
         <div className='flex flex-col bg-[#1c1b18] text-white text-left px-4 gap-2 py-4 '>
             <h1 className=' text-3xl font-bold'>Unify all your payments and revenue collection</h1>
             <p>Quis anim quis nisi ipsum velit amet eu ut enim est. Culpa ex labore esse ad dolor proident cupidatat officia fugiat eu sunt exercitation mollit non. Irure cupidatat ut amet laborum commodo enim irure reprehenderit commodo eu. Irure excepteur elit duis anim consequat qui.</p>
-            <button className='bg-[#F1F252] rounded-full text-[#1c1b18] py-1'>Sign Up</button>
-            <button className='border border-white rounded-full text-white py-1'>Contact Sales</button>
+            <button className='bg-[#F1F252] rounded-full text-[#1c1b18] py-2'>Sign Up</button>
+            <button className='border border-white rounded-full text-white py-2'>Contact Sales</button>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='bg-[#1c1b18] text-white top-0 py-2'>Keja</div>
-  )
+    <div className='bg-[#1c1b18] flex px-4 pt-4 justify-between '>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+      </svg>
+      <div className=' text-white top-0 py-2 text-2xl italic '>Keja.</div>
+      
+      </div>
+
+      )
 }
