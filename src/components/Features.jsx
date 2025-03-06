97fa29f36c1042d1b034127121146bfa1ec675b8
Pacheko <patrickoluochex@gmail.com>
2025-03-06 13:34:26 +0300

    update visa logo

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Pay from '../images/pay.svg'
+import Chat from '../images/chat.svg'
+import Paypal from '../images/paypal.svg'
+import Mpesa from '../images/mpesa.svg'
+import Visa from '../images/visa.svg'
 
 export default function Features() {
     return (
@@ -9,15 +13,20 @@ export default function Features() {
             <div className='flex flex-col '>
                 <h1 className='text-md font-bold '>Support Multiple Payment Gateways</h1>
                 <p>The mobile app allows your tenants to send payments easily and conviniently through our several supported payment providers.</p>
-                <img src={Pay} alt="payment" />
-            </div>
+                <div className="overflow-x-scroll flex gap-2">
+                    <img src={Paypal} alt="mpesa" className='flex-none size-40' />
+                    <img src={Mpesa} alt="mpesa" className='flex-none size-40' />
+                    <img src={Visa} alt="visa" className='flex-none size-40' />
+                </div>
 
-            <div>
-                <h1 className='text-md font-bold'>Interact seamlessly with your tenants and agents</h1>
-                <p>Bridge the gap between you and your tenants through a live chat.</p>
-            </div>
 
-            <div>
+                <div>
+                    <h1 className='text-md font-bold'>Interact seamlessly with your tenants and agents</h1>
+                    <p>Bridge the gap between you and your tenants through a live chat.</p>
+                    <img src={Chat} alt="payment" />
+                </div>
+
+                {/* <div>
                 <h1 className='text-md font-bold'>Enforce offers and deals to tenants</h1>
                 <p>Enforce offers and deals for your tenants to increase their stay .</p>
             </div>
@@ -25,7 +34,8 @@ export default function Features() {
             <div>
                 <h1 className='text-md font-bold'>Landlords Mobile App</h1>
                 <p>Manage your properties from the comfort of your bedroom through our mobile app .</p>
+            </div> */}
             </div>
-        </div>
-    )
+            </div>
+            )
 }
