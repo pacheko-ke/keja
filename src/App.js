02c5028659f5fb1bde4af699a2419540f2f2f1ce
Pacheko <patrickoluochex@gmail.com>
2025-03-06 16:35:58 +0300

    remove cookies consent

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Features from './components/Features';
 import Footer from './components/Footer';
 import RequestCall from './components/RequestCall';
+import Cookies from './components/Cookies';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Features/>
       <RequestCall/>
       <Footer/>
+      {/* <Cookies/> */}
       
     </div>
   );
