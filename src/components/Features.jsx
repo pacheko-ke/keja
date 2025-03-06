c7289d1b5a850f00edb1823b623c49f9dc30e2c2
Pacheko <patrickoluochex@gmail.com>
2025-03-06 13:15:17 +0300

    add features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Pay from '../images/pay.svg'
 
 export default function Features() {
     return (
-        <div className='mt-10 flex-col gap-2 mx-4'>
-            <h1 className='text-xl font-bold'>Manage Your Properties. With Zero Hassle</h1>
+        <div className='mt-10 flex flex-col mx-4 text-left gap-4 '>
+            <h1 className='text-xl font-bold mb-2'>Manage Your Properties.But With Zero Hassle</h1>
 
-            <div>
-                <h1 className='text-md font-bold'>Support Multiple Payment Gateways</h1>
-                <p>Our tenants mobile app allows tenants to send payments easily and conviniently.</p>
+            <div className='flex flex-col '>
+                <h1 className='text-md font-bold '>Support Multiple Payment Gateways</h1>
+                <p>The mobile app allows your tenants to send payments easily and conviniently through our several supported payment providers.</p>
+                <img src={Pay} alt="payment" />
             </div>
 
             <div>
@@ -18,6 +20,11 @@ export default function Features() {
             <div>
                 <h1 className='text-md font-bold'>Enforce offers and deals to tenants</h1>
                 <p>Enforce offers and deals for your tenants to increase their stay .</p>
+            </div>
+
+            <div>
+                <h1 className='text-md font-bold'>Landlords Mobile App</h1>
+                <p>Manage your properties from the comfort of your bedroom through our mobile app .</p>
             </div>
         </div>
     )
