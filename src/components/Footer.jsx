0a328bfd4da6320922eae7cf4dea9feeb15444c4
Pacheko <patrickoluochex@gmail.com>
2025-03-06 13:45:03 +0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='bottom-0 bg-[#F1F252] flex flex-col'>
-        
+    <div className='bottom-0 bg-[#F1F252] flex flex-col text-left pl-8 pt-2'>
+        <div className="flex flex-col pb-4">
+            <h1 className='font-bold'>How it works</h1>
+            <h1>Tenant</h1>
+            <h1>Landlord</h1>
+            <h1>Invoice Payments</h1>
+        </div>
     
     
     </div>
