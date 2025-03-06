d083cabe58f4586f31036f62e1e18c9e3f4b6ba9
Pacheko <patrickoluochex@gmail.com>
2025-03-06 16:05:55 +0300

    add feature under dev alert

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 export default function Home() {
+
+    const raiseAlert = ()=>{
+        alert("Feature under development")
+    }
     return (
         <div className='flex flex-col bg-[#1c1b18] text-white text-left px-4 gap-2 py-4 pt-20'>
             <h1 className=' text-3xl font-bold'>Property management, revenue collection and billing under one roof.</h1>
             <p>Quis anim quis nisi ipsum velit amet eu ut enim est. Culpa ex labore esse ad dolor proident cupidatat officia fugiat eu sunt exercitation mollit non. Irure cupidatat ut amet laborum commodo enim irure reprehenderit commodo eu. Irure excepteur elit duis anim consequat qui.</p>
-            <button className='bg-[#F1F252] rounded-full text-[#1c1b18] py-2'>Sign Up</button>
-            <button className='border border-white rounded-full text-white py-2'>Contact Sales</button>
+            <button className='bg-[#F1F252] rounded-full text-[#1c1b18] py-2' onClick={raiseAlert}>Sign Up</button>
+            <button className='border border-white rounded-full text-white py-2' onClick={raiseAlert}>Contact Sales</button>
         </div>
     )
 }
