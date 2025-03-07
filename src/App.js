34f20ae293d78870f74f4afc401ac9b4a87e3fa2
Pacheko <patrickoluochex@gmail.com>
2025-03-07 13:42:11 +0300

    add dashboard

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider, Route, Routes, HashRouter } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Features from './components/Features';
 import Footer from './components/Footer';
 import RequestCall from './components/RequestCall';
 import Cookies from './components/Cookies';
+import Landlord from './components/dashboard/Landlord';
+import Landing from './components/Landing';
 
 function App() {
   return (
+
+
     <div className="App">
-      <Navbar/>
-      <Home/>
-      <Features/>
-      <RequestCall/>
-      <Footer/>
-      {/* <Cookies/> */}
-      
+      <HashRouter>
+        <Routes>
+       
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Landlord />} />
+        </Routes>
+      </HashRouter>
+
     </div>
   );
 }
---
+++ src/components/dashboard/dashboardItems.js
@@ -0,0 +1,1 @@
+export const data = [ 'Overview', 'Performance','Campaigns','Orders','Products','Message','Sales Platform']
