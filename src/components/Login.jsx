c20f04b660691b8143a1e7870723eac1bf195b70
Pacheko <patrickoluochex@gmail.com>
2025-03-08 11:37:25 +0300

    create login screen

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+        <div className="flex flex-col text-left mx-4 mt-2">
+            <div className="flex justify-between">
+            <h1>Keja.</h1>
+           <h1>Login</h1>
+           </div>
+
+           <h1>Sign Up Today and Enjoy 1 Month Free Trial </h1>
+        </div>
+    </div>
   )
 }
