3ef7edf97a255d214cec4044f473d571dffc9756
Pacheko <patrickoluochex@gmail.com>
2025-03-14 16:32:32 +0300

    change accent color

--- src/components/dashboard/Newsletter.jsx
+++ src/components/dashboard/Newsletter.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Newsletter() {
   return (
-    <div className='flex gap-6 flex-col mx-10 mt-28 '>
-        <h1 className='text-4xl italic'>Keja<span className='text-green-400'>.</span></h1>
+    <div className='flex gap-4 flex-col mx-10 mt-28 '>
+        <h1 className='text-4xl italic'>Keja<span className='text-[#F1F252]'>.</span></h1>
         <h1 className='text-2xl'>Manage properties effortlessly</h1>
-        <p className="text-md">Sign up to get early access to Keja  </p>
-        <input type="email" placeholder='Enter email address' className='border pl-2 py-2 rounded-md invalid:border-red-500'/>
-        <button className='bg-green-500 py-2 rounded-md text-white'>Get Early Access</button>
+        <p className="text-sm">Sign up to get early access to Keja. We will mail you once the service launch  </p>
+        <input type="email" placeholder='Enter email address' className='border pl-2 py-2 rounded-md invalid:border-red-500 invalid:border-2'/>
+        <button className='bg-[#F1F252] py-2 rounded-md text-black'>Get Early Access</button>
 
 
 
