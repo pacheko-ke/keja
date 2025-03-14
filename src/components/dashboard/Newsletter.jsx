a0c4c986864973b54899afb80bf3121f5fee2b40
Pacheko <patrickoluochex@gmail.com>
2025-03-14 16:17:22 +0300

    add email sign up form

--- src/components/dashboard/Newsletter.jsx
+++ src/components/dashboard/Newsletter.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function Newsletter() {
   return (
-    <div>Newsletter
-        <h1>Keja</h1>
-        <h1 className='text-xl'>Manage properties effortlessly</h1>
+    <div className='flex gap-6 flex-col mx-10 mt-28 '>
+        <h1 className='text-4xl italic'>Keja</h1>
+        <h1 className='text-2xl'>Manage properties effortlessly</h1>
+        <p className="text-md">Sign up to get early access to Keja</p>
+        <input type="email" placeholder='Enter email address' className='border pl-2 py-2 rounded-md invalid:border-red-500'/>
+        <button className='bg-green-500 py-2 rounded-md text-white'>Get Early Access</button>
 
 
 
